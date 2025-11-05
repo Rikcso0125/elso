b644ef0555666bc42e421b8ea0cc029ad8f82b07
Rikcso0125 <ricsko.0125@gmail.com>
2025-11-05 23:06:24 +0100

    Update next.config.js to remove unnecessary config
    Removed experimental appDir configuration as it is now default.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true },
+  // Ebben a verzióban az App Router már alapértelmezett,
+  // az 'experimental: { appDir: true }' már nem szükséges.
+  // Itt lehetnek a további konfigurációk, ha vannak.
 };
+
 module.exports = nextConfig;
