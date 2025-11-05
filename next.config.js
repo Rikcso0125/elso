b54cbc7980a0b0635cf7e40359e2df24c9c1ce5c
Rikcso0125 <ricsko.0125@gmail.com>
2025-11-05 23:37:42 +0100

    Update next.config.js with configuration comments
    Added comments for future Next.js configurations.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true },
+  // Ide jöhetnek a további érvényes Next.js konfigurációk,
+  // pl. images, compiler, stb.
+  // De az 'experimental' szekciót most üresen hagyjuk vagy töröljük.
 };
+
 module.exports = nextConfig;
